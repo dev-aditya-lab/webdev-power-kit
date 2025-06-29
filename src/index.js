0a201b3da60ed2c23b1d01b50154e6b0a3d12a2c
adii <ad1123itya@gmail.com>
2025-06-29 11:11:18 +0530

    add preventClose.js implementation for tab close protection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-
 export * from './clipboard.js';
 export * from './notify.js';
 export * from './battery.js';
 export * from './vibration.js';
 export * from './network.js';
 export * from './tabVisibility.js';
+export * from './preventClose.js';
