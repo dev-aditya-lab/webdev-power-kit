1714051fee9f59ccb1539077a1f28eda12348a44
dev-aditya-lab <ad1123itya@gmail.com>
2026-07-14 16:45:26 +0530

    chore: update project dependencies and node_modules structure

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "img.shields.io" },
+      { protocol: "https", hostname: "raw.githubusercontent.com" },
+    ],
+  },
 };
 
 export default nextConfig;
